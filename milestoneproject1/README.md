2da4b59e3d336e1bc7ffed6cd304ad720142dbd2
Daniel van Duinkerken <daanivd@hotmail.com>
2019-03-09 15:03:24 +0000

    added milestoneproject1

---
+++ milestoneproject1/README.md
@@ -0,0 +1,73 @@
+# Daniel van Duinkerken Photography Website
+ 
+## Overview
+ 
+### What is this website for?
+ 
+This is a website to showcase the photography work of Daniel van Duinkerken.
+ 
+### What does it do?
+ 
+This website provides people with a portfolio of Daniel van Duinkerken's work, basic information about him, links to his social media accounts and ways to contact him
+ 
+### How does it work
+ 
+**JQUERY/JAVASCRIPT**  was used to create the gallery for image viewing and creating the modal for a contact form. **CSS** and **BOOTSTRAP** was used for styling.
+
+## Features
+ 
+### Existing Features
+- FRONT PAGE with large background image to draw the viewer in. Background image works both in landscape and portrait mode.
+- PORTFOLIO PAGE with a thumbnail gallery and filter for different categories. Clicking on a thumbnail displays the image larger and allows for clicking through all the images currently selected by a filter.
+    - Portfolio page is designed so no higher resolution images can be easily downloaded using 'right click-->save as'.   
+
+- INFO PAGE with basic information on Daniel van Duinkerken.
+- Contact Icon at top and bottom of page that open a modal to contact through a message (e-mail).
+- Social media icons that link to Instagram and Facebook accounts of Daniel van Duinkerken photography.
+  
+- Different resolution images are used for different screen resolutions for quicker loading of webpage.
+
+### Features Left to Implement
+- Enlarge image in Modal 
+
+## Tech Used
+
+### Some of the tech used includes:
+- **HTML**, **CSS** and **Javascript**
+  - Base languages used to create website
+- [Bootstrap](http://getbootstrap.com/)
+    - **Bootstrap** to give the project a simple, responsive layout
+- [JQuery](https://jquery.com)
+    - **JQuery** for gallery and modal contact form
+
+
+## Testing
+- Prototype code was written using editor of Cloud9
+- All code used on the site has been tested to ensure everything is working as expected
+- Site viewed and tested in the following browsers:
+  - Google Chrome
+  - Opera
+  - Microsoft Edge
+  - Mozilla Firefox
+  - Safari
+
+## Contributing
+The following websites links were used to help with coding:
+
+[full-screen-background-image-sizes-for-web-design](https://www.webmalama.com/the-best-full-screen-background-image-sizes-for-web-design/)
+[get-index-of-clicked-element-in-collection-with-jquery get index of selector](https://stackoverflow.com/questions/5545283/get-index-of-clicked-element-in-collection-with-jquery get index of selector)
+[how-to-change-image-opacity-on.html Hover on thumbnails](http://www.jquerybyexample.net/2012/05/how-to-change-image-opacity-on.html Hover on thumbnails)
+[contact-form-in-bootstrap-modal](http://reusableforms.com/d/p4/contact-form-in-bootstrap-modal)
+[bootstrap modal](https://www.w3schools.com/bootstrap/bootstrap_modal.asp)
+[blur filter css](https://stackoverflow.com/questions/20039765/how-to-apply-a-css-3-blur-filter-to-a-background-image)
+[keeping-a-navbar-ul-horizontal-on-small-screens](https://stackoverflow.com/questions/22385680/keeping-a-navbar-ul-horizontal-on-small-screens)
+[Markdown Cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+[Readme sample](https://code-institute-room.slack.com/files/U2D6L9DSS/F8X75A2V7/readme_md_sample.md)
+
+
+### Credits
+
+## Media
+- The photos and videos used onn this site are made by Daniel van Duinkerken. Audio files were downloaded from zapslat.com
+
+
---
+++ milestoneproject1/js/script.js
@@ -0,0 +1,202 @@
+//waits until page is ready
+$(document).ready(function() {
+
+    // FILTER BUTTONS //
+    //Change opacity of filter categories other than current hovered filter
+    $('.filter button').hover(function() {
+
+        var filter = $(this).attr('id');
+        $('.filter img').not('#' + filter).animate({ opacity: 0.5 }, 100);
+
+    }, function() {
+        $('.filter img').animate({ opacity: 1 }, 100);
+    });
+
+
+    //Only display Photo/video/audio selected by clicked filter
+    $('.filter button').click(function() {
+        var filter = $(this).attr('id');
+
+        if (filter == 'photo') {
+            $('.gallery').css('background-image', 'none');
+            $('.photo').css('display', 'block');
+            $('.audio').css('display', 'none');
+            $('.video').css('display', 'none');
+            $('.curImg').removeClass('curImg');
+            $('.gallery').css('height', '100%');
+            $('.top').css('display', 'block');
+            $('.scroll').css('display', 'none');
+            $('.filter button').animate({ opacity: 1 }, 100);
+            $('.photo').addClass('curFilter')
+            $('.video').removeClass('curFilter')
+            $('.audio').removeClass('curFilter')
+        }
+        
+        if (filter == 'video') {
+            $('.gallery').css('background-image', 'none');
+            $('.video').css('display', 'block');
+            $('.audio').css('display', 'none');
+            $('.photo').css('display', 'none');
+            $('.curImg').removeClass('curImg');
+            $('.gallery').css('height', '100%');
+            $('.top').css('display', 'block');
+            $('.scroll').css('display', 'none');
+            $('.filter button').animate({ opacity: 1 }, 100);
+            $('.video').addClass('curFilter')
+            $('.photo').removeClass('curFilter')
+            $('.audio').removeClass('curFilter')
+        }
+        
+        if (filter == 'audio') {
+            $('.gallery').css('background-image', 'none');
+            $('.audio').css('display', 'block');
+            $('.video').css('display', 'none');
+            $('.photo').css('display', 'none');
+            $('.curImg').removeClass('curImg');
+            $('.gallery').css('height', '100%');
+            $('.top').css('display', 'block');
+            $('.scroll').css('display', 'none');
+            $('.filter button').animate({ opacity: 1 }, 100);
+            $('.audio').addClass('curFilter')
+            $('.video').removeClass('curFilter')
+            $('.photo').removeClass('curFilter')
+        }
+        
+        
+       
+        //Fix footer to bottom when there are less than four thumbnails// 
+        numPics = $('.curFilter').length;
+
+        if (numPics < 4) {
+            $('.footer').addClass('navbar-fixed-bottom');
+        }
+        else if (numPics > 3) {
+            $('.footer').removeClass('navbar-fixed-bottom');
+        }
+    });
+
+    //HOVER OVER THUMBNAILS EFFECT
+    $('img.thumb').css('opacity', 1);
+
+    $('img.thumb').hover(function() {
+        $('.thumb').not(this).animate({ opacity: 0.5 }, 0);
+    }, function() {
+        $('.thumb').not(this).animate({ opacity: 1 }, 0);
+    });
+
+
+    // VIEW PHOTO LARGE //
+    $('.thumb').click(function() {
+        $(this).addClass('curImg');
+
+        if ($(window).width() < 960) {
+            imgURL = $('.curImg').attr('src').replace('/thumbs/', '/768px/')
+        }
+        else {
+            imgURL = $('.curImg').attr('src').replace('/thumbs/', '/1200px/')
+        }
+
+        //Close thumbnails/filter, open toggles for large photos
+        $('.thumb').css('display', 'none');
+        $('.gallery').css('height', '60vh');
+        $('.top').css('display', 'none');
+        $('.scroll').css('display', 'block');
+        $('.gallery').css('background-size', 'contain').css('background-image', 'url(' + imgURL + ')')
+        $('.footer').removeClass('navbar-fixed-bottom');
+    });
+
+
+    // CLOSE IMAGE
+    $('button.closeit').click(function() {
+        $('.gallery').css('background-image', 'none');
+        $('.thumb').css('display', 'block');
+        $('.thumb').addClass('curFilter');
+        $('.curFilter').css('display', 'block');
+        $('.curImg').removeClass('curImg');
+        $('.gallery').css('height', '100%');
+        $('.top').css('display', 'block');
+        $('.scroll').css('display', 'none');
+        $('.filter button').animate({ opacity: 1 }, 100);
+    })
+
+    // GO TO NEXT IMAGE 
+    $('button.next').click(function() {
+
+        var imgs = $('.curFilter');
+        var curImg = $('.curImg');
+        var pos = $('.curFilter').index(curImg);
+        var numPics = $('.curFilter').length;
+        var curImg = $('.curFilter')[pos];
+        var nextImg = $('.curFilter')[pos + 1];
+
+
+        //Loop current filter of photos
+        if (pos < numPics - 1) {
+            $(curImg).removeClass('curImg');
+            $(nextImg).addClass('curImg');
+        }
+
+        else {
+            $(curImg).removeClass('curImg');
+            var pos = 0;
+            nextImg = $('.curFilter')[pos];
+            $(nextImg).addClass('curImg');
+        }
+
+        if ($(window).width() < 960) {
+            imgURL = $('.curImg').attr('src').replace('/thumbs/', '/768px/')
+        }
+        else {
+            imgURL = $('.curImg').attr('src').replace('/thumbs/', '/1200px/')
+        }
+
+        $('.gallery').animate({
+            opacity: 0
+        }, 1000, function() {
+
+
+            $('.gallery').css('background-size', 'contain').css('background-image', 'url(' + imgURL + ')');
+            $('.gallery').animate({ opacity: 1 }, 1000);
+        });
+    });
+
+
+    // GO TO PREVIOUS IMAGE
+    $('button.previous').click(function() {
+        var imgs = $('.curFilter');
+        var curImg = $('.curImg');
+        var pos = $('.curFilter').index(curImg);
+        var numPics = $('.curFilter').length;
+        var curImg = $('.curFilter')[pos];
+        var nextImg = $('.curFilter')[pos - 1];
+
+        //Loop current filter of photos
+        if (pos > 0) {
+            $(curImg).removeClass('curImg');
+            $(nextImg).addClass('curImg');
+        }
+
+        else {
+            $(curImg).removeClass('curImg');
+            var pos = numPics - 1;
+            nextImg = $('.curFilter')[pos];
+            $(nextImg).addClass('curImg');
+        }
+
+        if ($(window).width() < 960) {
+            imgURL = $('.curImg').attr('src').replace('/thumbs/', '/768px/')
+        }
+        else {
+            imgURL = $('.curImg').attr('src').replace('/thumbs/', '/1200px/')
+        }
+
+        $('.gallery').animate({
+            opacity: 0
+        }, 1000, 'swing', function() {
+            $('.gallery').css('background-size', 'contain').css('background-image', 'url(' + imgURL + ')');
+            $('.gallery').animate({ opacity: 1 }, 1000, 'swing');
+        });
+
+    });
+
+});
